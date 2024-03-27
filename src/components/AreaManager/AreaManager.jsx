e0d92facf1cedbef34ec87c7d08c3d252a572ba8
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-27 17:56:03 +0100

    errors

--- src/components/AreaManager/AreaManager.jsx
+++ src/components/AreaManager/AreaManager.jsx
@@ -1,21 +1,35 @@
 import { Field, Form, Formik } from "formik";
 import styles from "./AreaManager.module.css";
+import * as Yup from "yup";
+
+const validationScheme = Yup.object({
+  area: Yup.string().required("required"),
+});
 
 const AreaManager = ({ addArea }) => {
-	const submitAddArea = (area, formActions) => {
-		addArea(area);
-		formActions.resetForm();
-	};
-	return (
-		<Formik initialValues={{ area: "" }} onSubmit={submitAddArea}>
-			<Form>
-				<Field className={styles.areaManagerInput} type="text" name="area" />
-				<button className={styles.areaManagerButton} type="submit">
-					add area
-				</button>
-			</Form>
-		</Formik>
-	);
+  const submitAddArea = (area, formActions) => {
+    addArea(area);
+    formActions.resetForm();
+  };
+  return (
+    <Formik
+      initialValues={{ area: "" }}
+      onSubmit={submitAddArea}
+      validationSchema={validationScheme}
+    >
+      <Form>
+        <Field
+          className={styles.areaManagerInput}
+          type="text"
+          name="area"
+          placeholder="AreaX"
+        />
+        <button className={styles.areaManagerButton} type="submit">
+          add area
+        </button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default AreaManager;
--- src/components/TechnicianManager/TechnicianManager.jsx
+++ src/components/TechnicianManager/TechnicianManager.jsx
@@ -1,25 +1,37 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 import styles from "./TechnicianManager.module.css";
 
+const validationScheme = Yup.object({
+  name: Yup.string().min(3, "name too short").required("required"),
+});
+
 const TechnicianManager = ({ area, addTechnician }) => {
-	const submitAddTechnician = (name, formActions) => {
-		addTechnician(name);
-		formActions.resetForm();
-	};
-	return (
-		<Formik initialValues={{ name: "" }} onSubmit={submitAddTechnician}>
-			<Form className={styles.technicianManagerContainer}>
-				<Field
-					className={styles.technicianManagerInput}
-					type="text"
-					name="name"
-				/>
-				<button className={styles.technicianManagerButton} type="submit">
-					add technician {area.area}
-				</button>
-			</Form>
-		</Formik>
-	);
+  const submitAddTechnician = (name, formActions) => {
+    addTechnician(name);
+    formActions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={{ name: "" }}
+      onSubmit={submitAddTechnician}
+      validationSchema={validationScheme}
+    >
+      <Form className={styles.technicianManagerContainer}>
+        <Field
+          className={styles.technicianManagerInput}
+          type="text"
+          name="name"
+          placeholder="John Doe"
+        />
+        <ErrorMessage className={styles.technicianManagerMessage} name="name" />
+        <button className={styles.technicianManagerButton} type="submit">
+          add technician {area.area}
+        </button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default TechnicianManager;
